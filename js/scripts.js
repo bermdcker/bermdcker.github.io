6e17d4699b1bb00fc5db1dd5e59abdaa5169e840
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2024-11-16 19:45:22 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,5 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// was originally blank In the original template
+console.log("%cWarning: Do not paste any code you don't know into the console.","color: white; background-color: red; font-size: 16px; padding: 10px; border-radius: 5px; font-weight: bold;"),console.log("%cPasting untrusted code can lead to:","color: yellow; font-size: 14px;"),console.log("%c- Data theft (cookies, passwords, session info)","color: orange; font-size: 14px;"),console.log("%c- Unauthorized actions (sending requests, stealing data)","color: orange; font-size: 14px;"),console.log("%c- Remote code execution (hacking your account, system)","color: orange; font-size: 14px;"),console.log("%c- Phishing attacks (fake login forms)","color: orange; font-size: 14px;"),console.log("%c- Website or application crashes (Denial of Service)","color: orange; font-size: 14px;"),console.log("%cAlways be cautious and run code from trusted sources!","color: yellow; font-size: 14px;");
